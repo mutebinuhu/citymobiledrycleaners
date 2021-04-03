9785e071961f5f7fd8f719143bcb7544455d356b
swaggp <mutebinuhu1@gmail.com>
2021-04-03 13:14:17 -0700

    few changes

--- app.js
+++ app.js
@@ -7,6 +7,6 @@ app.get('/', (req, res)=>{
     res.sendFile(path.join(__dirname+'/public/index.html'));
 });
 
-app.listen(process.env.PORT, ()=>{
+app.listen(process.env.PORT || 3000, ()=>{
     console.log("server started at port" + process.env.PORT);
 });
