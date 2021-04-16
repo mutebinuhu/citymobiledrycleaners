4a85b7e307507917f6ab4a429d729df186c065af
swaggp <mutebinuhu1@gmail.com>
2021-04-15 20:46:36 -0700

    displaying errors

--- Routes/api/sendrequest.js
+++ Routes/api/sendrequest.js
@@ -1,18 +1,30 @@
 const express = require('express');
 const Router = express();
 const Request = require('../../Models/Request')
+const path = require('path')
+
 const {body, validationResult} = require('express-validator')
-Router.post('/request',[
-    body('phone', 'Invalid Phone').not().isEmpty().isLength({
+/*returns the website view*/
+Router.get('/', (req, res)=>{
+    let details = {
+        name:"mutebi nuhu",
+        age:"43"
+    }
+    res.render('index',{details} )
+})
+Router.post('/',[
+    body('phone').not().isEmpty().withMessage('Phone number is required').isLength({
         min:7,
         max:12,
 
-    }).isMobilePhone()
+    }).withMessage('check phone characters').isMobilePhone().withMessage('Phone is invalid')
 ],
  (req, res)=>{
      let errors = validationResult(req);
    if (!errors.isEmpty()) {
-       res.status(400).json({errors:errors.array()})
+       let errorsList = errors.array();
+       console.log(errorsList);
+       res.render('index', {errorsList})
    }
    const {phone, message} = req.body;
    let request = new Request({
@@ -20,6 +32,9 @@ Router.post('/request',[
        message
    });
    request.save();
-   res.status(200).json({data:request})
+   res.render('index')
+
+
 });
+
 module.exports = Router;
--- app.js
+++ app.js
@@ -1,18 +1,32 @@
 require('dotenv').config();
 const express = require('express');
+const hbs = require('hbs');
 const dbConnection = require('./config/db/connection');
 dbConnection();
 const path = require('path')
 const app = express();
+app.set('view engine', 'hbs');
 app.use(express.json({extended: false}));
 app.use(express.urlencoded({ extended: true }));
-
 app.use(express.static('public'));
 //routes
 const requestRoute = require('./Routes/api/sendrequest')
-app.get('/', (req, res)=>{
-    res.sendFile(path.join(__dirname+'/public/index.html'));
-});
+
+app.get('/demo', (req, res)=>{
+    let demo = {
+        name:"nuhu",
+        age:20
+    }
+    let projects = {
+        name : 'Rahul', 
+        skills : ['Data Mining', 'BlockChain Dev', 'node.js']
+    }
+    res.render('demo', {
+        demo:demo,
+        projects:projects
+    })
+})
+
 app.use(requestRoute);
 app.listen(process.env.PORT || 3000, ()=>{
     console.log("server started at port" + process.env.PORT);
