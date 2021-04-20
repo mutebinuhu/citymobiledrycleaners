b869ce51a041519dc083254e86415659dcc398a5
swaggp <mutebinuhu1@gmail.com>
2021-04-20 05:59:07 -0700

    adding new info

--- Routes/Requests/index.js
+++ Routes/Requests/index.js
@@ -1,8 +1,7 @@
 const express = require('express');
 const Router = express();
 const Request = require('../../Models/Request')
-const path = require('path')
-
+const path = require('path');
 const {body, validationResult} = require('express-validator')
 /*returns the website view*/
 Router.get('/', (req, res)=>{
@@ -40,5 +39,18 @@ Router.post('/',[
    res.render('index', {successMessage})
 
 });
+
+//method GET
+//description Gett all requests
+//access private
+Router.get('/requests',  async (req, res)=>{
+    try {
+        let requests = await Request.find();
+        res.status(200).json(requests)
+    } catch (error) {
+        console.log(err.message)
+        res.status(500).send("server error")
+    }
+})
 
 module.exports = Router;
