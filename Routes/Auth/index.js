f7966c51fbbf41e89f79faa12e7c26582fa692f9
swaggp <mutebinuhu1@gmail.com>
2021-05-17 00:10:13 -0700

    adding validation to signup form

--- Routes/Auth/index.js
+++ Routes/Auth/index.js
@@ -1,8 +1,57 @@
 const express = require('express');
 const Router = express();
-Router.get('/signup', (req, res)=>{
+const bcrypt = require('bcryptjs');
+const bodyParser = require('body-parser');
+const User = require('../../Models/User');
+const {body, validationResult} = require('express-validator')
+
+
+Router.get('/signup', async (req, res)=>{
+    
     res.render('signup')
 });
+Router.post('/signup',
+body('name', 'Name is required').not().isEmpty(),
+body('email', 'Email required with minimum of 6 characters').isEmail(),
+body('password', 'Password is required').not().isEmpty()
+, async (req, res)=>{
+    let errors = validationResult(req);
+    const {name, email, password, confirm_password} = req.body;
+
+    if(!errors.isEmpty()){
+        let errorsList = errors.array()
+        res.render('signup', {errorsList})
+        console.log(errors)
+    }
+    if(confirm_password !== password){
+        let confirmPasswordError = "Passwords Dont Match";
+        res.render('signup', {confirmPasswordError})
+    }else{
+        try {
+            let user = await User.findOne({email});
+            if(user){
+                let data = "User exists"
+                res.render('signup', {data})
+            }
+           
+     
+            user = new User({
+                name,
+                email,
+                password
+            })
+            let salt = await bcrypt.genSalt(10)
+            user.password =  await bcrypt.hash(password, salt)
+            await user.save()
+     
+         } catch (error) {
+            console.log(error.message) 
+         }
+         res.render('signup')
+    }
+    
+})
+
 Router.get('/login', (req, res)=>{
     res.render('login')
 });
---
+++ Routes/api/users.js
@@ -0,0 +1,3 @@
+const express = require('express');
+const Router = express.Router();
+const {body, validationResult} = require('express-validator');
