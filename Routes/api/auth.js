7ed9bfa7c5df4ef0bf58f2cc6169a96955dc3a1f
swaggp <mutebinuhu1@gmail.com>
2021-05-18 05:26:10 -0700

    creating the login api

--- Routes/api/auth.js
+++ Routes/api/auth.js
@@ -1,13 +1,53 @@
 const express = require('express');
 const Router = express();
 const auth = require('../../middleware/auth');
-const User = require('../../Models/User')
+const User = require('../../Models/User');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config')
+//create  token
 Router.get('/', auth, async (req, res)=>{
    try {
        const user = await User.findById(req.user.id).select('-password')
        res.status(200).json(user)
    } catch (err) {
        console.log(err.message)
+       res.status(500).send("Server Error");
    }
 });
+//login
+Router.post('/', async (req, res)=>{
+
+    try {
+        const {email, password} = req.body;
+        let user = await User.findOne({email})
+        if(!user){
+            res.status(400).json({msg:"Invalid Credentials"})
+        }
+        const isMatch = await bcrypt.compare(password, user.password);
+        if(!isMatch){
+            res.status(400).json({msg:"Invalid Credentials"})
+        }
+         //create jwt payload
+         const payLoad = {
+            user:{
+                id:user.id
+            }
+        }
+        //sign the payload
+        jwt.sign(
+            payLoad,
+            config.get('jwtToken'),
+            {expiresIn:36000},
+            (err, token)=>{
+                if (err) throw err;
+                res.status(200).json({token})
+            }
+        )
+
+    } catch (err) {
+        console.log(err.message)
+        res.status(500).send("server error")
+    }
+})
 module.exports = Router;
