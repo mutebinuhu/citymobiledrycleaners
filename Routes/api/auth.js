be9d7f677112717bfe2b13e139956084d0495440
swaggp <mutebinuhu1@gmail.com>
2021-05-18 04:24:13 -0700

    few changes

--- Routes/api/auth.js
+++ Routes/api/auth.js
@@ -4,7 +4,7 @@ const auth = require('../../middleware/auth');
 const User = require('../../Models/User')
 Router.get('/', auth, async (req, res)=>{
    try {
-       const user = await User.findById(req.user.id)
+       const user = await User.findById(req.user.id).select('-password')
        res.status(200).json(user)
    } catch (err) {
        console.log(err.message)
