c2aad24b3e3a359a0089206c73c737cdec890ae8
swaggp <mutebinuhu1@gmail.com>
2021-06-17 04:10:47 -0700

    displaying errors on the screen

--- Routes/Home/index.js
+++ Routes/Home/index.js
@@ -1,17 +1,18 @@
 const express = require('express');
 const Router = express();
-
+const {body, validationResult} = require('express-validator')
 Router.get('/', (req, res)=>{
     res.render('index')
 });
-Router.post('/',
+Router.post('/',[body('phone', 'Phone Number is Required').not().isEmpty(), body('phone', 'Phone number should not be less than 10').isLength({min: 10}).trim().escape()],
  async (req, res)=>{
-     //let errors = validationResult(req);
-   //if (!errors.isEmpty()) {
-       //let errorsList = errors.array();
-       //console.log(erro rsList);
-       //res.status(400).json({errors:errorsList})
-   //}
+     let errors = validationResult(req);
+    if (!errors.isEmpty()) {
+       let errorsList = errors.array();
+       res.render('index',{
+           errors: errorsList
+       })
+   }
    const {message, phone} = req.body;
    try {
     let request = new Request({
