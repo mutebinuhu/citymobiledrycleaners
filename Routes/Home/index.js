e2754b4101a84393d83025548417a9c5d36c1aff
swaggp <mutebinuhu1@gmail.com>
2021-06-21 03:00:34 -0700

    few changes

--- Routes/Home/index.js
+++ Routes/Home/index.js
@@ -28,5 +28,29 @@ Router.post('/request',[body('phone', 'Phone Number is Required').not().isEmpty(
    }
   
 });
+Router.get('/request',[body('phone', 'Phone Number is Required').not().isEmpty(), body('phone', 'Phone number should not be less than 10').isLength({min: 10}).trim().escape()],
+ async (req, res)=>{
+     let errors = validationResult(req);
+    if (!errors.isEmpty()) {
+       let errorsList = errors.array();
+       res.render('index',{
+           errors: errorsList
+       })
+   }
+   const {message, phone} = req.body;
+   try {
+    let request = new Request({
+        message,
+        phone 
+    });
+   await request.save();
+   res.render('index', {
+     data: true  
+   })
+   } catch (err) {
+       console.log(err.message)
+   }
+  
+});
 
 module.exports = Router;
